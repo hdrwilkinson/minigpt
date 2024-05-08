79ab68dbedd1b1b19180c6c2c8845b2a41d2a8c8
hdrw-instadeep <137811825+hdrw-instadeep@users.noreply.github.com>
2024-05-08 17:06:01 +0100

    refactor: initial init files for pytorch implementation

---
+++ minigpt/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This module contains the implementation of the MiniGPT package.
+MiniGPT is a minimalistic implementation of the Generative Pre-trained Transformer (GPT) model.
+
+We will have 3 implementations of the model:
+1. PyTorch implementation (https://github.com/karpathy/nanoGPT, 
+https://github.com/karpathy/ng-video-lecture, https://www.youtube.com/watch?v=kCc8FmEb1nY)
+2. JAX implementation (https://github.com/mgrankin/minGPT/)
+3. Flax implementation (https://github.com/brentyi/minGPT-flax)
+"""
